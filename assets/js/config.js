ff25caf7f6dadc7e3aa4c32aaf91f2b88f65726e
emerald-stock-exchange <manta.ray.young@gmail.com>
2026-01-01 10:56:40 -0800

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+// assets/js/config.js
+const GOOGLE_CLIENT_ID = '739471109344-5ebditnjrtk83sqpr0e28bsu90vjps4t.apps.googleusercontent.com';
+const APPS_SCRIPT_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby5Zatwi2ko_KmQ0YkzNMNFlruoJ6IjKxUDZE70_r0eKF5Ok72enwuVuaXWL39Q-v6iPg/exec';
